1eb8b4a03a26c3c6de73ea64e058a3d7ecc9ab8a
martapanc <marta.panc@gmail.com>
2021-02-17 20:06:48 +0000

    Fix Json response in Health endpoint

--- src/main/kotlin/com/pancaldim/bookwishlistapi/controller/HealthController.kt
+++ src/main/kotlin/com/pancaldim/bookwishlistapi/controller/HealthController.kt
@@ -1,18 +1,20 @@
 package com.pancaldim.bookwishlistapi.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
 class HealthController {
 
-    @GetMapping("/health")
+    @RequestMapping(value = ["/health"], produces = ["application/json"], method = [RequestMethod.GET])
     fun getHealthStatus(): Status {
         return getStatus()
     }
 
-    @GetMapping("/")
+    @RequestMapping(value = ["/"], produces = ["application/json"], method = [RequestMethod.GET])
     fun getHomePage(): Status {
         return getStatus()
     }
